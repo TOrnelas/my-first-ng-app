c5be4323f4934000f2118a5fccd9d8dfa79a9f2e
Tiago Ornelas <tiago@front.no>
2018-11-27 11:24:06 +0000

    Github pages fifth deployment - image placeholder path fix

--- src/app/pipes/poster-image.pipe.ts
+++ src/app/pipes/poster-image.pipe.ts
@@ -8,6 +8,6 @@ export class PosterImagePipe implements PipeTransform { // todo maybe refactor t
   transform(value: string) {
     if (value)
       return 'https://image.tmdb.org/t/p/w500' + value;
-    else return '/assets/poster_placeholder.png'
+    else return '/my-first-ng-app/assets/poster_placeholder.png'
   }
 }
