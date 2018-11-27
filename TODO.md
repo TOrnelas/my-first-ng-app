78d057ff1ffd235419a256beea30b143c8a137d9
Tiago Ornelas <tiago@front.no>
2018-11-27 14:53:18 +0000

    Some general fixes

--- TODO.md
+++ TODO.md
@@ -3,3 +3,8 @@
 - Pagination without thumbnail click
 - 404 page
 - Images lazy loading
+- Back to top icon button on content list component
+- ActiveLink class on side menu icon loaded
+- Add about menu entry
+- Favourites?
+- Details page for movies and shows
--- src/app/pipes/poster-image.pipe.ts
+++ src/app/pipes/poster-image.pipe.ts
@@ -7,7 +7,7 @@ export class PosterImagePipe implements PipeTransform { // todo maybe refactor t
 
   transform(value: string) {
     if (value)
-      return 'https://image.tmdb.org/t/p/w500' + value;
+      return 'https://image.tmdb.org/t/p/w300' + value;
     else
       return '/assets/poster_placeholder.png' // todo figure out with /my-first-ng-app/ needs to be prepended when deploying
   }
