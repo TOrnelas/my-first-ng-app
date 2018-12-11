bb1fd0fa619c5af98d2985c6a81ed56c70737c57
Tiago Ornelas <tiago@front.no>
2018-12-11 17:56:43 +0000

    Github pages deploy

--- src/app/pipes/poster-image.pipe.ts
+++ src/app/pipes/poster-image.pipe.ts
@@ -9,6 +9,6 @@ export class PosterImagePipe implements PipeTransform { // todo maybe refactor t
     if (value)
       return 'https://image.tmdb.org/t/p/w300' + value;
     else
-      return 'assets/poster_placeholder.png' // todo figure out with /my-first-ng-app/ needs to be prepended when deploying
+      return '/my-first-ng-app/assets/poster_placeholder.png' // todo figure out with /my-first-ng-app/ needs to be prepended when deploying
   }
 }
